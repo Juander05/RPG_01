dfe867f66b86798ca4695929d2f808947fd66a9c
Juander05 <juandrobles0504@gmail.com>
2025-09-29 01:37:12 -0600

    Proyecto RPG

---
+++ Assets/Scripts/MovPlayer.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class MovPlayer : MonoBehaviour
+{
+
+    private Vector2 dirMov;
+    public float velMov;
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    void FixedUpdate(){
+        Movimiento();
+        AnimacionesPlayer();
+    }
+
+    private void Movimiento(){
+        float movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+        dirMov = new Vector2( movX, movY ).normalized;
+        rb.linearVelocity = new Vector2 (dirMov.x * velMov ,dirMov.y * velMov);
+    }
+
+    private void AnimacionesPlayer(){
+        anim.SetFloat("movX", dirMov.x);
+        anim.SetFloat("movY", dirMov.y);
+
+    }
+
+
+}
