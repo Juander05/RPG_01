d26a40d4724ba0013cac27afb1b65525d05b3f06
Juander05 <juandrobles0504@gmail.com>
2025-10-20 15:33:22 -0600

    Enemigos y ataques del Player

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo : MonoBehaviour
+{
+	
+	public static int vidaEnemigo = 1;
+	private float frecAtaque = 2.5f, tiempoSigAtaque = 0, iniciaConteo;
+	
+	public Transform personaje;
+	private NavMeshAgent agente;
+	public Transform[] puntosRuta;
+	private int indiceRuta = 0;
+	private bool playerEnRango = false;
+	[SerializeField] private float distanciaDeteccionPlayer;
+	private SpriteRenderer spriteEnemigo;
+	private Transform mirarHacia;
+	
+	private Animator animator;
+	private Vector2 ultimaDireccion;
+	
+	private void Awake(){
+		agente = GetComponent<NavMeshAgent>();
+		spriteEnemigo = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
+	}
+	
+    void Start(){
+	    vidaEnemigo = 1;
+	    agente.updateRotation = false;
+	    agente.updateUpAxis = false;
+    }
+
+	void Update(){
+    	
+		this.transform.position = new Vector3(transform.position.x, transform.position.y,0);
+		float distancia = Vector3.Distance(personaje.position, this.transform.position);
+		
+		//Animacion programada con los puntoRuta
+		//this.transform.position == puntosRuta[indiceRuta].position
+		if(Vector3.Distance(transform.position, puntosRuta[indiceRuta].position) < 0.2f){
+			if(indiceRuta < puntosRuta.Length -1){
+				indiceRuta++;
+			}else if(indiceRuta == puntosRuta.Length -1){
+				indiceRuta = 0;
+			}
+		}
+		
+		if(distancia < distanciaDeteccionPlayer){
+			playerEnRango = true;
+		} else {
+			playerEnRango = false;
+		}
+    	
+	    if(tiempoSigAtaque > 0){
+	    	tiempoSigAtaque = frecAtaque + iniciaConteo - Time.time;
+	    } else{
+	    	tiempoSigAtaque = 0;
+	    	VidasPlayer.puedePerderVida = 1;
+	    	SigueAlPlayer(playerEnRango);
+	    	RotaEnemigo();
+	    	ActualizaAnimacion();
+	    }
+	}
+    
+	private void SigueAlPlayer(bool playerEnRango){
+		//Sigue al player
+		if(playerEnRango){
+			agente.SetDestination(personaje.position);
+			mirarHacia = personaje;
+		} else{
+			//Regresa a su ruta
+			agente.SetDestination(puntosRuta[indiceRuta].position);
+			mirarHacia = puntosRuta[indiceRuta];
+		}
+	}
+	
+	private void RotaEnemigo(){
+		if(this.transform.position.x > mirarHacia.position.x){
+			spriteEnemigo.flipX = true;
+			Debug.Log("FlipX");
+		} else {
+			spriteEnemigo.flipX = false;
+			Debug.Log("Sin FlipX");
+		}
+	}
+	
+	private void ActualizaAnimacion()
+	{
+		// Calculamos la dirección de movimiento
+		Vector3 velocidad = agente.velocity;
+
+		float movX = velocidad.x;
+		float movY = velocidad.y;
+
+		// Pasamos los valores al Animator
+		animator.SetFloat("movX", movX);
+		animator.SetFloat("movY", movY);
+
+		// Determinamos si está en movimiento
+		bool estaCaminando = velocidad.magnitude > 0.05f;
+		animator.SetBool("isWalking", estaCaminando);
+
+		// Guardamos la última dirección (opcional, si luego quieres idle mirando hacia una dirección)
+		if (estaCaminando)
+		{
+			ultimaDireccion = new Vector2(movX, movY);
+		}
+	}
+    
+    
+	private void OnTriggerEnter2D(Collider2D obj){
+		if(obj.tag == "Player"){
+			tiempoSigAtaque = frecAtaque;
+			iniciaConteo = Time.time;
+			obj.transform.GetComponentInChildren<VidasPlayer>().TomarDaño(1);
+		}
+	}
+	
+	public void TomarDaño(int daño){
+		vidaEnemigo -= daño;
+		if(vidaEnemigo <= 0){
+			Destroy(gameObject);
+		}
+	}
+    
+}
---
+++ Assets/Scripts/MovPlayer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+
+public class MovPlayer : MonoBehaviour
+{
+
+    private Vector2 dirMov;
+    public float velMov;
+    public Rigidbody2D rb;
+	public Animator anim;
+    
+	private string capaIdle = "Idle";
+	private string capaCaminar = "Caminar";
+	private bool PlayerMoviendose = false;
+	private float ultimoMovX, ultimoMovY;
+	
+	public static int dirAtaque = 0;	// 1-Front, 2-Back, 3-Left, 4-Right
+
+    void FixedUpdate(){
+	    Movimiento();
+	    if(CCC.atacando == false && CAD.disparando == false){
+	    	AnimacionesPlayer();
+	    }
+    }
+
+    private void Movimiento(){
+        float movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+        dirMov = new Vector2( movX, movY ).normalized;
+	    rb.linearVelocity = new Vector2 (dirMov.x * velMov ,dirMov.y * velMov);
+	    
+	    if(movX == -1){
+	    	dirAtaque = 3;
+	    }
+	    
+	    if(movX == 1){
+	    	dirAtaque = 4;
+	    }
+	    
+	    if(movY == -1){
+	    	dirAtaque = 1;
+	    }
+	    
+	    if(movY == 1){
+	    	dirAtaque = 2;
+	    }
+        
+	    if(movX == 0 && movY == 0){		//Idle
+	    	PlayerMoviendose = false;
+	    } else{		//Caminar
+	    	PlayerMoviendose = true;
+	    	ultimoMovX = movX;
+	    	ultimoMovY = movY;
+	    }
+	    
+	    ActualizaCapa();
+        
+    }
+
+    private void AnimacionesPlayer(){
+	    anim.SetFloat("movX", ultimoMovX);
+	    anim.SetFloat("movY", ultimoMovY);
+
+    }
+    
+	private void ActualizaCapa(){
+		if(CCC.atacando == false && CAD.disparando == false){
+			if(PlayerMoviendose){
+				ActivaCapa(capaCaminar);
+				Debug.Log("Caminando");
+			} else{
+				ActivaCapa(capaIdle);
+				Debug.Log("Idle");
+			}
+		} else {
+			ActivaCapa("Ataque");
+		}
+	}
+	
+	private void ActivaCapa(string nombre){
+		for(int i=0; i < anim.layerCount; i++){
+			anim.SetLayerWeight(i, 0);		//Ambos layers con weight en 0
+		}
+		
+		anim.SetLayerWeight(anim.GetLayerIndex(nombre), 1);
+	}
+
+
+}
