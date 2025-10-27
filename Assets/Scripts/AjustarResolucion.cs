235812fe0c41def766310ebf5734f6824827b07b
Juander05 <juandrobles0504@gmail.com>
2025-10-26 18:01:22 -0600

    Generic RPG III

---
+++ Assets/Scripts/AjustarResolucion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AjustarResolucion : MonoBehaviour
+{
+	void Start()
+	{
+		int ancho = Display.main.systemWidth;
+		int alto = Display.main.systemHeight;
+
+		Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+		Screen.SetResolution(ancho, alto, true);
+
+		Debug.Log($"Resolución ajustada a: {ancho}x{alto}");
+	}
+}
